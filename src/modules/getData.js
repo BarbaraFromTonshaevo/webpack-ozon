8fffdc09fa9892ec53682ec94fb8d2fb16738ee2
Elkina Barbara <elkinavarvara1999@gmail.com>
2026-09-25 13:44:24 +0300

    fix: load goods from local db/db.json instead of dead Firebase

--- src/modules/getData.js
+++ src/modules/getData.js
@@ -1,6 +1,11 @@
+// Firebase-база интенсива больше недоступна (401), поэтому товары берём из db/db.json.
+// new URL(..., import.meta.url) говорит webpack положить файл в сборку и подставить его адрес
+const dbUrl = new URL('../../db/db.json', import.meta.url)
+
 const getData = () => {
-    return fetch('https://o-zone-3474c-default-rtdb.firebaseio.com/goods.json')
+    return fetch(dbUrl)
       .then(response => response.json())
+      .then(data => data.goods)
 }
 
 export default getData
